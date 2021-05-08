8ce970dd0c9ad3e573ac21ed6a50285b7df24a3d
e0611349 <e0611349@gmail.com>
2021-05-09 00:00:34 +0900

    plus form

--- blog/models.py
+++ blog/models.py
@@ -31,5 +31,5 @@ class Comment(models.Model):
     def __str__(self):
         return self.text
 
-    def approved_comments(self):
+    def approved_comment(self):
         return self.comments.filter(approved_comment=True)
